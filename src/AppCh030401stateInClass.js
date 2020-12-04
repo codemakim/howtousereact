08c6870c58a903ba20f33842a159c6f7d0fee2d2
ibiz-jhkim23 <codemakim@gmail.com>
2020-12-04 13:08:29 +0900

    클래스형 컴포넌트에서 setState 사용하기 끝

--- src/AppCh030401stateInClass.js
+++ src/AppCh030401stateInClass.js
@@ -32,8 +32,33 @@ class AppCh030401stateInClass extends Component {
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-            this.setState({number: number + 1});
+            //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다. 비동기적으로 업데이트 됩니다.
+            //this.setState({number: number + 1});
+
+            // prevState(첫번째 파라미터 | 기존 state 값)을 사용하면
+            // onClick 함수 내에서 여러번 state 값을 변경할 수 있음.
+            // 두번째 파라미터로 props를 넣을 수 있는데, (setState(prevState, props){...}) 
+            // 그 파라미터는 별도의 인자를 넘겨받을 때 사용할 수 있다.
+            this.setState(prevState => {
+              return {
+                number: prevState.number + 1
+              };
+            });
+
+            this.setState(prevState => ({
+              number: prevState.number + 1
+            }));
+
+            // setState의 두번째 파라미터로 콜백 함수를 넘겨주면 setState 동작이 끝난 후
+            // 해당 함수의 동작을 실행할 수 있다.
+            // *** 중요! 
+            // setState 사용시 prevState 값을 넘겨주는 것과 그렇지 않을 때의 동작 차이가 매우 크다. 이 점을 숙지해야한다.
+            this.setState({
+              number: number + 1,
+            }, () => {
+              console.log('방금 setState가 호출되었습니다.');
+              console.log(this.state);
+            });
           }}
         >+1</button>
       </div>
