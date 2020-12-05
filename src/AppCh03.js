d15902c291d4d23f5e7ab75cf32189e32d5c8865
ibiz-jhkim23 <codemakim@gmail.com>
2020-12-05 16:47:06 +0900

    3장 학습 완료

--- src/AppCh03.js
+++ src/AppCh03.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import AppCh030401stateInClass from "./AppCh030401stateInClass";
+import AppCh030402useStateInFUnc from "./AppCh030402useStateInFUnc";
+import AppCh03423manyUseState from "./AppCh03423manyUseState";
 
 function AppCh03() {
 
   // 숫자 값을 props로 넘기는 코드 예. <AppCh0301 name={3}>리액트</AppCh0301>
   // return <AppCh0301 name="React">리액트</AppCh0301>
-  return <AppCh030401stateInClass/>;
+  return (
+    <>
+      <h1>클래스형 컴포넌트에서 state 사용</h1>
+      <AppCh030401stateInClass/>
+      <hr/>
+      <h1>함수형 컴포넌트에서 useState 사용</h1>
+      <AppCh030402useStateInFUnc/>
+      <hr/>
+      <h1>한 컴포넌트에서 useState 여러 번 사용하기</h1>
+      <AppCh03423manyUseState/>
+    </>
+  );
 }
 
 export default AppCh03;
--- src/AppCh030402useStateInFUnc.js
+++ src/AppCh030402useStateInFUnc.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AppCh030402useStateInFUnc() {
+  /**
+   * useState 함수의 인자에는 상태의 초깃값을 넣어준다. 현재 코드는 빈문자열.
+   * useState에서 값의 형태는 클래스 컴포넌트틔 state와 달리 객체가 아니어도 된다.
+   * 함수를 호출하면 배열이 반환된다.
+   * 배열의 첫번째 요소는 현재 상태
+   * 배열의 두번째 요소는 상태를 바꿔주는 setter 함수
+   * 아래와 같이 배열 비구조화 할당을 통해 이름을 자유롭게 정할 수 있다.
+   */
+  const [message, setMessage] = useState('');
+  const onClickEnter = () => setMessage('안녕하세요!');
+  const onClickLeave = () => setMessage('안녕히 가세요!');
+
   return (
     <div>
-      
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1>{message}</h1>
     </div>
   )
 }
