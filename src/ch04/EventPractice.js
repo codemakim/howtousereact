24bfe5d1fc96b7ec2580d2c1bac9af713cd629f0
ibiz-jhkim23 <codemakim@gmail.com>
2020-12-08 13:11:06 +0900

    함수형 컴포넌트에서 여러 Input의 이벤트를 핸들링하는 방법

--- src/ch04/EventPractice.js
+++ src/ch04/EventPractice.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function EventPractice(props) {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = e => setUsername(e.target.value);
+  const onCChangeMessage = e => setMessage(e.target.value);
+
+  const onClick = () => {
+    alert(username + ' : ' + message);
+    setUsername('');
+    setMessage('');
+  };
+
+  const onKeyPress = e => {
+    if(e.key === 'Enter') {
+      onClick();
+    }
+  };
+
   return (
     <>
       <h1>이벤트 연습</h1>
-      <input 
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
         type="text"
         name="message"
-        placeholder="아무거나 입력해보세요."
-        onChange={
-          (e) => {
-            console.log(e.target.value);
-          }
-        }
+        placeholder="아무거나 입력해보세요"
+        value={message}
+        onChange={onCChangeMessage}
+        onKeyPress={onKeyPress}
       />
+      <button onClick={onClick}>확인</button>
     </>
   );
 }
